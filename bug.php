ed1d6489f97166573227dc2b492e8997e6cba441
BugHunter-X <askzenai@gmail.com>
2024-12-24 12:04:06 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,14 @@
+function foo(array $arr) {
+  foreach ($arr as $key => $value) {
+    if ($value === 'bar') {
+      unset($arr[$key]);
+    }
+  }
+  return $arr;
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
+
+//The bug is that foreach loop modifying the array during iteration can lead to unexpected results. When the element 'bar' is unset, the keys shift, causing the loop to skip elements.
